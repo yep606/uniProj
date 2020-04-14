5fb2e4a10e6b406380a50f1b107d46f80d79db9c
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-14 23:27:42 +0300

    fix

--- code.js
+++ code.js
@@ -1,9 +1,13 @@
-var ws = new WebSocket('ws://localhost:15674/ws');
-var client = Stomp.over(ws);
+function connect(){
+    var ws = new WebSocket('ws://localhost:15674/ws');
+    var client = Stomp.over(ws);
+    client.connect('guest', 'guest', on_connect, on_error, '/');
+}
+
 var on_connect = function() {
     console.log('connected');
 };
 var on_error =  function() {
     console.log('error');
 };
-client.connect('guest', 'guest', on_connect, on_error, '/');
+
