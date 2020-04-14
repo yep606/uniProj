d693bc1ae7a4b522227441bf761d56aa05027051
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-14 22:45:32 +0300

    stompjs

--- code.js
+++ code.js
@@ -1,20 +1,9 @@
-var mapArr = document.getElementsByClassName("map");
-
-var selectedId = 0;
-
-for(let i = 1; i < mapArr.length; i++){
-
-    mapArr[i].classList.add("hidden");
-
-}
-
-function change(){
-
-    var newSelectedId = document.getElementById("levels").value;
-
-    mapArr[selectedId].classList.add("hidden");
-    mapArr[newSelectedId].classList.remove("hidden");
-    selectedId = newSelectedId;
-        
-}
-
+var ws = new WebSocket('ws://localhost:15674/ws');
+var client = Stomp.over(ws);
+var on_connect = function() {
+    console.log('connected');
+};
+var on_error =  function() {
+    console.log('error');
+};
+client.connect('guest', 'guest', on_connect, on_error, '/');
