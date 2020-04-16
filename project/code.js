7f4e4284fc2fd99dd49a36d97174c6e2b0c67697
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-16 20:10:28 +0300

    more

---
+++ project/code.js
@@ -0,0 +1,36 @@
+var client;
+const destination = "/topic/test";
+const chat = document.getElementById("chat");
+const connectBtn = document.getElementById("connectBtn");
+const messageArea = document.getElementById("messageArea");
+var subscription;
+
+function connect(){
+    var ws = new WebSocket('ws://localhost:15674/ws');
+    client = Stomp.over(ws);
+    client.connect('guest', 'guest', on_connect, on_error, '/');
+}
+
+function sendMessage(){
+
+    client.send(destination, {}, "Hello, STOMP!");
+
+}
+
+var on_connect = function() {
+    subscription = client.subscribe(destination, callback);
+    connectBtn.classList.add("hidden");
+    chat.classList.remove("hidden");
+};
+var on_error =  function() {
+    console.log('error');
+};
+
+var callback =  function(message) {
+
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(message.body));
+  messageArea.appendChild(li);
+
+};
+
