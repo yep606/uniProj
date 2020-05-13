1b3afae20a75a390366fb4e9702eb4a2f2f6e3b5
yep606 <ilya.rogov.vladislav@gmail.com>
2020-05-14 00:50:07 +0300

    start

--- project/code.js
+++ project/code.js
@@ -1,19 +1,30 @@
-Vue.component('todo-items', {
-    props: ['list'],
-    template: '<div> {{list.name}}</div>'
+Vue.component('message-list', {
+    props: ['messages'],
+    template: '<div>' + 
+    '<input> </input>' + '<button v-on:click="add">Add</button>' + 
+    '<div v-for="message in messages">'
+        + '{{message.name}} ' +
+        '</div>' +
+        '</div>',
+    methods: {
+        add : function(){
+            this.messages.push({name: 'New one', id: 1});
+        }
+    },
 })
 
 var app = new Vue({
     el: '#app',
+    template: '<message-list v-bind:messages="messages"></message-list>',
     data: {
-        items: [
+        messages: [
 
-            {name: 'James', id: 1},
-            {name: 'Alex', id: 2},
-            {name: 'Ilay', id: 3},
+            { name: 'James', id: 1 },
+            { name: 'Alex', id: 2 },
+            { name: 'Ilay', id: 3 },
 
         ]
     }
-  })
+})
 
 
