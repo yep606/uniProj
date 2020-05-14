338ff837736887f07dacc79a36c04298fc21399e
yep606 <ilya.rogov.vladislav@gmail.com>
2020-05-14 16:19:47 +0300

    stage

--- project/code.js
+++ project/code.js
@@ -1,30 +1,103 @@
-Vue.component('message-list', {
-    props: ['messages'],
-    template: '<div>' + 
-    '<input> </input>' + '<button v-on:click="add">Add</button>' + 
-    '<div v-for="message in messages">'
-        + '{{message.name}} ' +
-        '</div>' +
-        '</div>',
-    methods: {
-        add : function(){
-            this.messages.push({name: 'New one', id: 1});
+Vue.component('message-form', {
+
+    data: function() {
+        return {
+            text: '',
+            id: '',
         }
     },
-})
+    
+    props: ['messages', 'message'],
 
-var app = new Vue({
-    el: '#app',
-    template: '<message-list v-bind:messages="messages"></message-list>',
-    data: {
-        messages: [
+    template: '<div>' +
+     '<input placeholder="Write a messsge" v-model="text" />'+
+     '<button @click="save">Send</button>'
+    + '</div>',
 
-            { name: 'James', id: 1 },
-            { name: 'Alex', id: 2 },
-            { name: 'Ilay', id: 3 },
+    watch: {
+        message: function(newValue){
+            this.text = newValue.text;
+            this.id = newValue.id;
+        }
+    },
+    methods: {
+        save: function(){
+            var message = {text: this.text, id: this.id}
+            if(this.id){ //update
+                var index = this.messages.findIndex(elem => elem.id === this.id)
+                this.messages.splice(index, 1, message);
+                this.text = '';
+                this.id ='';
+            }
+            else{ //adding
+                this.messages.push({text: this.text, id: 55})
+                this.text ='';
+            }
+        
+        }
 
-        ]
     }
 })
 
+Vue.component('message-row', {
+    props: ['messages', 'message', 'editMessage'],
+    template: '<div>' +
+    '{{message.text}} {{message.id}}' + 
+    '<button @click="edit">Edit</button>' +
+    '</div>',
 
+    methods: {
+
+        edit: function(){
+            this.editMessage(this.message);
+        }
+
+    }
+
+})
+
+Vue.component('message-list', {
+    data: function(){
+
+        return {
+            message: null,
+        }
+
+    },
+    props: ['messages'],
+    template: '<div>' +
+    '<message-row v-for="message in messages" :key="message.id"' +
+     ':messages="messages" :message="message" :editMessage="editMessage">' +
+    '</message-row>' +
+    '<message-form :messages="messages" :message="message" />'
+    + '</div>',
+
+    methods: {
+
+        editMessage: function(message){
+            this.message = message;
+        }
+
+    }
+});
+
+new Vue({
+    el: '#app',
+    template: '<message-list :messages="messages"></message-list>',
+    data: {
+        messages: [
+            {text: 'alex1', id:1},
+            {text: 'alex2', id:2},
+            {text: 'alex3', id:3}
+        ],
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            })
+            this.newTodoText = ''
+        }
+    }
+})
