aca3013432052a32eea42a8b3734d1c3ef53655a
yep606 <ilya.rogov.vladislav@gmail.com>
2020-08-04 21:20:11 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './project/code.js',
+  entry: './project/ax.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
